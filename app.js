f301c3bcd7d5d3eeccb041b09899d1eec0d15bd6
Will Knowles <will@willknowles.com>
2021-08-09 16:16:07 -0500

    Generate HTML File

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-  
-printProfileData(profileDataArgs);
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
